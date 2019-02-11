94273bb1bd3b764689179c64e39c5dbd0e6db0bc
JasonLomsdalen <jlomsdal@calpoly.edu>
2019-02-10 17:01:26 -0800

    Added Scene and scene change scripts

---
+++ GravityUnity/Assets/Scripts/MoonScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoonScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("Moon");
+        Physics.gravity = new Vector3(0.0f, -1.62f, 0.0f);
+        Debug.Log("Hello World!");
+    }
+
+}
