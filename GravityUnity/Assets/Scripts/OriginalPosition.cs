3121a1fe10502de4902be97b77f61925a87705a0
JasonLomsdalen <jlomsdal@calpoly.edu>
2019-02-20 00:47:54 -0800

    Lots of things

---
+++ GravityUnity/Assets/Scripts/OriginalPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OriginalPosition : MonoBehaviour
+{
+    Vector3 startPos;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void resetBalls ()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform.position = startPos;
+        if (rb)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
